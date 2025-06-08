301d40101dd5a2095a9376c32215ed6abb4623bd
KlientGuy <princeofnarf@gmail.com>
2025-06-08 13:36:41 +0200

    fix

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/')]
 #[Route('/{_locale}')]
 class AppController extends AbstractController
 {
